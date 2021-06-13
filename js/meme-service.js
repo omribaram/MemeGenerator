c17d8a8c45aabb0905792db25660686ae59e115b
omribaram <84462496+omribaram@users.noreply.github.com>
2021-06-14 02:06:27 +0300

    Add files via upload

--- js/meme-service.js
+++ js/meme-service.js
@@ -142,17 +142,23 @@ function setKeyword(value) {
 }
 
 function shareMeme() {
-    fetch(getFinalCanvas()).then(function(blob) {
-        var file = new File([blob], "meme.jpg", { type: 'image/jpeg' });
-        var filesArray = [file];
-        if (navigator.canShare && navigator.canShare({ files: filesArray })) {
-            navigator.share({
-                text: 'Check out my meme!',
-                files: filesArray,
-                title: 'Check out my meme!',
-            });
-        }
-    })
+    fetch(getFinalCanvas())
+        .then(function(response) {
+            return response.blob()
+        })
+        .then(function(blob) {
+
+            var file = new File([blob], "meme.png", { type: 'image/png' });
+            var filesArray = [file];
+
+            if (navigator.canShare && navigator.canShare({ files: filesArray })) {
+                navigator.share({
+                    text: 'Check out my meme!',
+                    files: filesArray,
+                    title: 'Check out my meme!',
+                });
+            }
+        })
 }
 
 
